f506755f5a2a57d1befd65b14268a741f9193918
Angel Yang <angel10132433@gmail.com>
2024-05-15 19:09:45 -0400

    homework 2 submission

--- 03_homework/homework_2.sql
+++ 03_homework/homework_2.sql
@@ -1,19 +1,26 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
-
+SELECT * from customer;
 
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
-
-
+SELECT * 
+from customer
+ORDER by customer_last_name, customer_first_name
+LIMIT 10;
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
-
+SELECT * 
+from customer_purchases
+where product_id IN (4, 9)
 -- option 2
+SELECT * 
+from customer_purchases
+where product_id  = 4 OR product_id = 9
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
 filtered by vendor IDs between 8 and 10 (inclusive) using either:
@@ -21,10 +28,15 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
-
+SELECT *, (quantity*cost_to_customer_per_qty) as price 
+from customer_purchases
+where product_id >= 8 
+and product_id <= 10
 
 -- option 2
-
+SELECT *, (quantity*cost_to_customer_per_qty) as price 
+from customer_purchases
+where product_id between 8 and 10
 --CASE
 /* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
 Using the product table, write a query that outputs the product_id and product_name
@@ -32,11 +44,22 @@ columns and add a column called prod_qty_type_condensed that displays the word �
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 
 
+SELECT product_id, product_name,
+(CASE WHEN product_qty_type = "unit" THEN "unit" ELSE "bulk" END) as product_qty_type
+FROM product
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 
+SELECT product_id, product_name,
+(CASE WHEN product_qty_type = "unit" THEN "unit" ELSE "bulk" END) as product_qty_type,
+(CASE WHEN product_name like "%Pepper%" THEN 1 ELSE 0 END) as pepper_flag
+FROM product
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
+SELECT *
+FROM vendor v INNER JOIN vendor_booth_assignments vb 
+ON v.vendor_id = vb.vendor_id
+ORDER by vendor_name, market_date
