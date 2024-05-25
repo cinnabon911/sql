e6fc0d91c8bff210dcc9e5b051263c802e3ad3e4
Angel Yang <angel10132433@gmail.com>
2024-05-24 22:33:51 -0400

    homework 5 submission

--- 03_homework/homework_5.sql
+++ 03_homework/homework_5.sql
@@ -1,33 +1,3 @@
--- String manipulations
-/* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
-These are separated from the product name with a hyphen. 
-Create a column using SUBSTR (and a couple of other commands) that captures these, but is otherwise NULL. 
-Remove any trailing or leading whitespaces. Don't just use a case statement for each product! 
-
-| product_name               | description |
-|----------------------------|-------------|
-| Habanero Peppers - Organic | Organic     |
-
-Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
-
-
-
-/* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
-
-
-
--- UNION
-/* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
-
-HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following: 
-1) Create a CTE/Temp Table to find sales values grouped dates; 
-2) Create another CTE/Temp table with a rank windowed function on the previous query to create 
-"best day" and "worst day"; 
-3) Query the second temp table twice, once for the best day, once for the worst day, 
-with a UNION binding them. */
-
-
-
 -- Cross Join
 /*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
 customer on record. How much money would each vendor make per product? 
@@ -38,7 +8,12 @@ Remember, CROSS JOIN will explode your table rows, so CROSS JOIN should likely b
 Think a bit about the row counts: how many distinct vendors, product names are there (x)?
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
-
+SELECT sum(5*original_price) as sales, vendor_name, product_name FROM
+(SELECT DISTINCT vendor_name, product_name, original_price
+from vendor_inventory as vi LEFT JOIN vendor as v on vi.vendor_id = v.vendor_id
+LEFT JOIN product p on vi.product_id = p.product_id) sub
+CROSS JOIN customer
+GROUP by vendor_name, product_name
 
 
 -- INSERT
@@ -46,18 +21,27 @@ Before your final group by you should have the product of those two queries (x*y
 This table will contain only products where the `product_qty_type = 'unit'`. 
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
-
+CREATE TEMP TABLE product_units
+as 
+SELECT *, CURRENT_TIMESTAMP as snapshot_timestamp FROM product
+WHERE product_qty_type = 'unit'
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
-
+INSERT INTO product_units
+VALUES (7, 'Apple Pie', '10"', 3, 'unit', CURRENT_TIMESTAMP)
 
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
+DELETE FROM product_units
+WHERE snapshot_timestamp = (
+SELECT max(snapshot_timestamp)
+FROM product_units
+)
 
 
 
@@ -78,4 +62,16 @@ Finally, make sure you have a WHERE statement to update the right row,
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
+UPDATE product_units 
+SET current_quantity = (
+SELECT latest_qty from 
+(SELECT pu.product_id, coalesce(quantity, 0) latest_qty from product_units pu 
+left join (SELECT v.market_date, v.product_id, v.quantity
+from vendor_inventory as v INNER JOIN
+(SELECT product_id, max(market_date) as maxdate
+FROM vendor_inventory
+group by product_id) as sub
+on v.market_date = sub.maxdate and v.product_id = sub.product_id) as outsub
+on pu.product_id = outsub.product_id) as sub2
+WHERE sub2.product_id = product_units.product_id)
 
